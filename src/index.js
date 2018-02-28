b48b3ac546f658548178e26314621b5d24ec2c36
Anton <anton@sobes.io>
2018-02-28 22:37:23 +0000

    tests and bin

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
-/**
- * Invoke package's main function
- */
-function yarnUp() {
-    console.log('yarn-up called')
+const spawncommand = require('spawncommand')
+
+async function run(packageJson) {
+  if (packageJson === null || packageJson === undefined) {
+    throw new Error('Package.json content is not given')
+  }
+  const { devDependencies, dependencies } = packageJson
+  const keys = Object.keys({ ...devDependencies, ...dependencies })
+    .map(s => `${s}@latest`)
+
+  const proc = spawncommand('yarn', ['upgrade', ...keys], {
+    stdio: 'inherit',
+  })
+  await proc.promise
 }
 
-module.exports = yarnUp
+module.exports = run
--- test/spec/index.js
+++ test/spec/index.js
@@ -1,17 +1,28 @@
-const assert = require('assert')
-const context = require('../context/')
-const yarnUp = require('../../src/')
+const spanwcommand = require('spawncommand')
+const { resolve } = require('path')
+const { throws, equal } = require('zoroaster/assert')
+const context = require('../context')
+const yarnUp = require('../..')
+
+process.env.ZOROASTER_TIMEOUT = 3000
 
 const yarnUpTestSuite = {
-    context,
-    'should be a function': () => {
-        assert.equal(typeof yarnUp, 'function')
-    },
-    'should call package without error': () => {
-        assert.doesNotThrow(() => {
-            yarnUp()
-        })
-    },
+  context,
+  'should be a function'() {
+    equal(typeof yarnUp, 'function')
+  },
+  async 'should call package without error'() {
+    await throws({
+      fn: yarnUp,
+      message: 'Package.json content is not given',
+    })
+  },
+  async 'spawns the bin'() {
+    const p = spanwcommand(resolve(__dirname, '../../bin/yarn-up'), [], {
+      stdio: [0, 1, 2],
+    })
+    await p.promise
+  },
 }
 
 module.exports = yarnUpTestSuite
