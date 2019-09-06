08a67e5ca46af10c255feb573b6446d4684683ca
Anton <anton@adc.sh>
2019-09-06 23:05:10 +0300

    c131

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,9 @@
 ## 6 September 2019
 
+### [1.3.1](https://github.com/artdecocode/yarn-up/compare/v1.3.0...v1.3.1)
+
+- [fix] Fix spawn.
+
 ### [1.3.0](https://github.com/artdecocode/yarn-up/compare/v1.2.0...v1.3.0)
 
 - [feature] Add the `-u` option to automatically upgrade.
--- src/bin/yarn-up.js
+++ src/bin/yarn-up.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import { resolve } from 'path'
-import spawn from 'spawncommand'
+import { spawn } from 'child_process'
 import { _exclude, _upgrade } from './get-args'
 import up from '../'
 
@@ -9,10 +9,7 @@ const packageJson = require(resolve(process.cwd(), 'package.json'))
 ;(async () => {
   try {
     await up(packageJson, _exclude ? _exclude.split(',') : undefined)
-    if (_upgrade) {
-      const p = spawn('yarn', ['upgrade'])
-      await p.promise
-    }
+    if (_upgrade) spawn('yarn', ['upgrade'])
   } catch (err) {
     console.log(err)
   }
