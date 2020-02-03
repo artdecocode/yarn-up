a676c184f5713aeb218ccc662416d17ba92b8353
Anton <anton@sobesednik.media>
2020-02-03 06:36:49 +0300

    help

--- src/bin/get-args.js
+++ src/bin/get-args.js
@@ -10,7 +10,18 @@ export const argsConfig = {
     boolean: true,
     short: 'u',
   },
+  'help': {
+    description: 'Display usage information.',
+    boolean: true,
+    short: 'h',
+  },
+  'version': {
+    description: 'Show version number.',
+    boolean: true,
+    short: 'v',
+  },
 }
+
 const args = argufy(argsConfig)
 
 /**
@@ -22,6 +33,16 @@ export const _exclude = /** @type {string} */ (args['exclude'])
  * Run `yarn upgrade` after the update.
  */
 export const _upgrade = /** @type {boolean} */ (args['upgrade'])
+
+/**
+ * Display usage information.
+ */
+export const _help = /** @type {boolean} */ (args['help'])
+
+/**
+ * Show version number.
+ */
+export const _version = /** @type {boolean} */ (args['version'])
 
 /**
  * The additional arguments passed to the program.
--- src/bin/yarn-up.js
+++ src/bin/yarn-up.js
@@ -1,8 +1,24 @@
 #!/usr/bin/env node
 import { resolve } from 'path'
 import { spawn } from 'child_process'
-import { _exclude, _upgrade } from './get-args'
+import { reduceUsage } from 'argufy'
+import usually from 'usually'
+import { _exclude, _upgrade, _help, _version, argsConfig } from './get-args'
 import up from '../'
+
+if (_help) {
+  const usage = usually({
+    description: 'Upgrade all dependencies to the next version.',
+    example: 'yarn-up -e preact -u',
+    line: 'yarn-up [-e packageA,packageB] [-u] [-hv]',
+    usage: reduceUsage(argsConfig),
+  })
+  console.log(usage)
+  process.exit(0)
+} else if (_version) {
+  console.log(require('../../package.json').version)
+  process.exit(0)
+}
 
 const packageJson = require(resolve(process.cwd(), 'package.json'))
 
